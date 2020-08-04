cf9c0c614181f9152e422ba5aae75143fdcc8c2e
Marques <marquesde2013@gmail.com>
2020-08-03 23:06:57 -0300

    component PageHeader+ Styles

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import PageHeader from '../../components/PageHeader';
 
 function TeacherForm(){
 
     return(
-        <h1>Teacher Form</h1>
+        <div id="page-teacher-form" className="container">
+            <PageHeader title="Que incrivel que você qier dar aulas."/>
+        </div>
         )
 }
 export default TeacherForm;
