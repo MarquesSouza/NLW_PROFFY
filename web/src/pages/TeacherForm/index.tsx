0aa6e75f5b761f96eb93e75dcf41157bcc6cb96d
Marques <marquesde2013@gmail.com>
2020-08-05 20:39:39 -0300

    Input Component

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
 
+import './styles.css';
+import Input from '../../components/Input';
+
 function TeacherForm(){
 
     return(
         <div id="page-teacher-form" className="container">
-            <PageHeader title="Que incrivel que você qier dar aulas."/>
+            <PageHeader title="Que incrivel que você qier dar aulas."
+                description="O primerio passo é preencher esse formulário de inscrição"
+            />
+            <main>
+                <fieldset>
+                    <legend>Seus Dados</legend>
+                    <Input name="name" label="Nome Completo"/>
+                    <Input name="avatar" label="Avatar"/>
+                    <Input name="whatsapp" label="Whatsapp"/>
+                    
+                </fieldset>
+                <fieldset>
+                    <legend>Seus Dados</legend>
+                    <Input name="name" label="Nome Completo"/>
+                    <Input name="avatar" label="Avatar"/>
+                    <Input name="whatsapp" label="Whatsapp"/>
+                    
+                </fieldset>
+            </main>
         </div>
         )
 }
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import './styles.css';
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input';
 
 
 function TeacherList(){
@@ -11,19 +12,11 @@ function TeacherList(){
             <div id="page-teacher-list" className="container">
                 <PageHeader title="Estes são os proffys disponiveis.">
                     <form id="search-teachers">
-                        <div className="input-block">
-                            <label htmlFor="subject">Matéria</label>
-                            <input type="text" id="subject"/>
-                        </div>
-                        <div className="input-block">
-                            <label htmlFor="week_day">Dia da semana</label>
-                            <input type="text" id="week_day"/>
-                        </div>
-                        <div className="input-block">
-                            <label htmlFor="time">Hora</label>
-                            <input type="text" id="time"/>
-                        </div>
-                    </form>
+                        <Input name="subject" label="Matéria"/>
+                        <Input name="week_day" label="Dia da semana"/>
+                        <Input type="time" name="time" label="Hora"/>
+                        
+                       </form>
                 </PageHeader>
                 <main>
                   <TeacherItem />
