78838d64c843b9263e2848b566b0607abd508b70
Marques <marquesde2013@gmail.com>
2020-08-04 23:24:48 -0300

    connection componentes and cors

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,59 +1,15 @@
 import express from 'express';
-import db from './database/connection';
-import convertHourToMinutes from './utils/convertHourToMinutes';
+import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsControllers';
 
 const routes =express.Router();
+const classesControllers=new ClassesController();
+const connectionsController=new ConnectionsController();
 
-interface ScheduleItem{
-    week_day:number;
-    from:string;
-    to:string;
-}
-routes.post('/classes',async (request,response)=>{
-    const {
-        name,
-        avatar,
-        whatsapp,
-        bio,
-        subject,
-        cost,
-        schedule
-    }= request.body;
-    
-    const trx = await db.transaction();
-    
-    try{
-        const insertedUsersId =await trx('users').insert({
-            name,
-            avatar,
-            whatsapp,
-            bio,
-        })
-        const user_id= insertedUsersId[0];
-        const insertedClassesId = await trx('classes').insert({
-            subject,
-            cost,
-            user_id,
-    
-        })
-        const class_id=insertedClassesId[0];
-        const classSchedule=schedule.map((scheduleItem:ScheduleItem)=>{
-            return{
-                class_id,
-                week_day:scheduleItem.week_day,
-                from:convertHourToMinutes(scheduleItem.from),
-                to:convertHourToMinutes(scheduleItem.to),
-            };
-        })
-        await trx('class_schedule').insert(classSchedule);
-        await trx.commit();
-        return response.status(201).send();
-    }catch(err){
-        await trx.rollback();
-        return response.status(400).json({
-            error:'Unexprected error while creating new class'
-        })
-    }
-});
+routes.post('/classes', classesControllers.create);
+routes.get('/classes', classesControllers.index);
+
+routes.post('/connections',connectionsController.create)
+routes.get('/connections',connectionsController.index)
 
 export default routes;
