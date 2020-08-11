75da7f63f3a0017e599cef78e9763c9132265766
Marques <marquesde2013@gmail.com>
2020-08-11 20:39:12 -0300

    page login

--- web/src/components/Input/index.tsx
+++ web/src/components/Input/index.tsx
@@ -4,12 +4,11 @@ import warningIcon from '../../assets/images/icons/warning.svg';
 import './styles.css';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement>{
-    label:string;
     name:string;
     password?:boolean;
 }
 
-const Input: React.FC<InputProps>= ({label,name,password, ...rest})=>{
+const Input: React.FC<InputProps>= ({name,password, ...rest})=>{
     const [isVisiblePassword,setIsVisiblePassword]=useState(password)
     
     function handleToggleVisiblePassword(){
@@ -17,8 +16,7 @@ const Input: React.FC<InputProps>= ({label,name,password, ...rest})=>{
     }
     return(
         <div className="input-block">
-            <label htmlFor="subject">{label}</label>
-            <input type={password?"password":"text"} id={name} {...rest} />
+           <input type={isVisiblePassword?"password":"text"} id={name} {...rest} />
            
         </div>
     )
--- web/src/pages/LoginForm/index.tsx
+++ web/src/pages/LoginForm/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 import './styles.css';
 import Banner from '../../components/Banner';
 import Input from '../../components/Input';
@@ -11,10 +12,34 @@ function LoginForm(){
                    
                     <fieldset>
                         <legend>Fazer Login</legend>
-                        <Input name="email" label="E-mail"/>
-                        <Input name="senha" label="Senha" password={true} />
+                        <Input name="email" placeholder="E-mail"/>
+                        <Input name="senha" placeholder="Senha" password={true} />
                     </fieldset>
+                    <fieldset>
+                        <div className="option-block">
+                            <div className="checkbox-block">
+                                <input type="checkbox" className="checkbox"/>
+                                <label >Lembrar-me</label>
+                            </div>
+                             <Link to="/recoverpassword" className="RecoverPassword"> Esqueci minha senha </Link>
+                             <button type="submit">Entrar</button>
+                        </div>
+                    </fieldset> 
                 </form>
+                <footer>
+                    <div className="footer-block">
+                        <div className="cadastro-block">
+                            <label htmlFor="" className="cadastro-label">
+                                Não tem cadastro?
+                            </label>
+                            <Link to="/cadastro"className="cadastro-link">Cadastre-se</Link>
+                        </div>
+                        <label  className="messagem">
+                            E de graça
+                            <img src="" alt=""/>
+                        </label>
+                    </div>
+                </footer>
             </main>
         </div>
     );
